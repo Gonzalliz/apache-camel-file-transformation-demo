7ecccde764f6c8148f0b48729a43060543381dd9
Chamara Liyanage <chamara@Chamaras-MacBook-Air.local>
2024-11-19 08:50:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# apache-camel-file-transformation-demo
+
+The demonstration is showing how to create a basic application that can read a JSON file content and transform, write to a CSV file
+
+Video walkthrough of the application - [link](https://youtu.be/-OoPvstDgAE)
---
+++ src/main/java/com/demo/blueprint/FileTransformationDemo.java
@@ -0,0 +1,13 @@
+package com.demo.blueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTransformationDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileTransformationDemo.class, args);
+    }
+
+}
